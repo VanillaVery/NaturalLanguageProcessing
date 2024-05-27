896ad6462038bd9e0cb8c4369592469e0b223789
yoonyoojin <dbsdbwls0602@naver.com>
2024-05-27 23:56:06 +0900

    토크나이저 mecab설치 실패후 kiwi로 전환...

--- 임베딩/word2vec.py
+++ 임베딩/word2vec.py
@@ -7,4 +7,29 @@
 #word2vec은 밀집 표현을 위해 CBoW와 Skip-gram이라는 두 가지 방법을 사용 
 
 #%%
-# CBoW : 주변에 있는 단어를 가지고 중간에 있는 단어를 예측
+#네거티브 샘플링 :전체 단어 집합에서 일부 단어를 샘플링하여 오답 단어로 사용
+
+"""# 모델 실습: Skip-Gram"""
+# 단어의 수 V, 임베딩 차원을 E -> W(V,E) , W'(E,V)를 최적화 하며 학습
+# 임베딩 클래스를 사용하여 룩업 계층을 구현해보자
+
+#%%
+# 기본 SKIP GRAM 클래스
+from torch import nn
+
+class VanillaSkipgram(nn.Module):
+    def __init__(self,vocab_size , embedding_dim):
+        super().__init__()
+        self.embedding = nn.Embedding(
+            num_embedding = vocab_size,
+            embedding_dim = embedding_dim
+        )
+        self.linear = nn.Linear(
+            in_features = embedding_dim,
+            out_features =  vocab_size
+        )
+
+def foward(self,input_ids):
+    embedding = self.embedding(input_ids)
+    output = self.linear(embedding)
+    return output
