b0437c703de1867201d0f491a45273bb4b0b3745
yoonyoojin <dbsdbwls0602@naver.com>
2024-06-06 23:41:52 +0900

    lstm 구조 학습

--- 임베딩/rnn.py
+++ 임베딩/rnn.py
@@ -37,3 +37,57 @@ h_0 = torch.rand(num_layers * (int(bidirectional) + 1), batch_size,output_size) 
 outputs, hidden = model(inputs, h_0)
 print(outputs.shape) #torch.Size([4, 6, 512])
 print(hidden.shape) #torch.Size([6, 4, 256])
+#%%
+#LSTM
+import torch
+from torch import nn
+lstm = torch.nn.LSTM(
+    input_size,
+    hidden_size,
+    num_layers=1,
+    bias=False,
+    batch_first=True,
+    dropout=0,
+    bidirectional=False,
+    proj_size=0 # >0일 시, 출력차원을 줄이거나 변환 가능 
+)
+
+#%%
+# 다양한 구조의 lstm 구성 가능
+# 양방향 다층 장단기 메모리 구성 
+
+import torch
+from torch import nn
+
+input_size = 128
+output_size = 256
+num_layers = 3
+bidirectional = True
+proj_size = 64
+
+model = nn.LSTM(
+    input_size=input_size,
+    hidden_size=output_size,
+    num_layers=num_layers,
+    batch_first=True,
+    bidirectional=bidirectional,
+    proj_size=proj_size
+)
+
+batch_size = 4
+sequence_len = 6
+
+inputs = torch.randn(batch_size, sequence_len, input_size)
+h_0 = torch.rand(
+    num_layers * (int(bidirectional) + 1),
+    batch_size,
+    proj_size if proj_size > 0 else output_size
+)
+c_0 = torch.rand(num_layers * (int(bidirectional) +1), batch_size, output_size)
+
+outputs, (h_n,c_n) = model(inputs, (h_0,c_0))
+
+print(outputs.shape) # torch.Size([4, 6, 128])
+print(h_n.shape) # torch.Size([6, 4, 64])
+print(c_n.shape) # torch.Size([6, 4, 256])
+
