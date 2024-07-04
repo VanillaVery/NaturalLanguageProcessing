a8e0649ad176068a3339a6cacb660b33bbf3c322
yoonyoojin <dbsdbwls0602@naver.com>
2024-07-04 23:32:34 +0900

    트랜스포머 구조 실습

--- 트랜스포머/Transformer.py
+++ 트랜스포머/Transformer.py
@@ -63,14 +63,14 @@ token_transform = {
 
 
 print("Token Transform:")
-print(token_transform)
+print(token_transform) # 토크나이저 저장
 
-#vocab_transform:토큰을 인덱스로 변환시키는 함수를 저장
+# build_vocab_from_iterator : 토큰-> 단어 집합 생성 
 vocab_transform = {}
 for language in [SRC_LANGUAGE,TGT_LANGUAGE]:
     train_iter = Multi30k(split="train", language_pair=(SRC_LANGUAGE,TGT_LANGUAGE))
-    vocab_transform[language] = build_vocab_from_iterator(
-        generate_tokens(train_iter,language),
+    vocab_transform[language] = build_vocab_from_iterator( #토큰-> 인덱스
+        generate_tokens(train_iter,language), #텍스트->토큰
         min_freq=1,
         specials=special_symbols,
         special_first=True,
@@ -80,5 +80,103 @@ for language in [SRC_LANGUAGE,TGT_LANGUAGE]:
     vocab_transform[language].set_default_index(UNK_IDX)
 
 print("Vocab Transform:")
-print(vocab_transform)
-# 아이디어를 정리
+print(vocab_transform) # 단어 집합
+
+# 요약: 각각 말뭉치에 대해 토크나이저와 어휘사전을 생성
+#%%
+# 트랜스포머 모델 구성 
+import math
+import torch
+from torch import nn
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len, dropout=0.1):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0,d_model,2)*(-math.log(10000.0)/d_model)
+            )
+        pe = torch.zeros(max_len,1,d_model)
+        pe[:,0,0::2] = torch.sin(position * div_term)
+        pe[:,0,1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe",pe)
+
+    
+    def forword(self,x):
+        x = x + self.pe[: x.size(0)]
+        return self.dropout(x)
+
+class TokenEmbedding(nn.Module):
+    def __init__(self, vocab_size, emb_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, emb_size)
+        self.emb_size = emb_size
+
+    def forward(self,tokens):
+        return self.embedding(tokens.long())*math.sqrt(self.emb_size)
+
+class Seq2SeqTransformer(nn.Module):
+    def __init__(
+            self,
+            num_encoder_layers,
+            num_decoder_layers,
+            emb_size,
+            max_len,
+            nhead,
+            src_vocab_size,
+            tgt_vocab_size,
+            dim_feedforward,
+            dropout=0.1
+    ):
+        super().__init__()
+        self.src_tok_emb = TokenEmbedding(src_vocab_size,emb_size)
+        self.tgt_tok_emb = TokenEmbedding(tgt_vocab_size, emb_size)
+        self.positional_encoding = PositionalEncoding(
+            d_model=emb_size,max_len=max_len,dropout=dropout
+        )
+        self.transformer = nn.Transformer(
+            d_model=emb_size,
+            nhead=nhead,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dim_feedforward = dim_feedforward,
+            dropout=dropout,
+        )
+        self.generator = nn.Linear(emb_size, tgt_vocab_size)
+    
+        def forward(
+                self,
+                src,
+                trg,
+                src_mask,
+                tgt_mask,
+                src_padding_mask,
+                tgt_padding_mask,
+                memory_key_padding_mask,
+        ):
+            src_emb = self.positional_encoding(self.src_tok_emb(src))
+            tgt_emb = self.positional_encoding(self.tgt_tok_emb(trg))
+            outs = self.transformer(
+                src=src_emb,
+                tgt=tgt_emb,
+                src_mask=src_mask,
+                tgt_mask=tgt_mask,
+                memory_mask=None,
+                src_key_padding_mask = src_padding_mask,
+                tgt_key_padding_mask = tgt_padding_mask,
+                memory_key_padding_mask = memory_key_padding_mask
+            )
+            return self.generator(outs)
+        
+        def encode(self, src, src_mask):
+            return self.transformer.encoder(
+                self.positional_encoding(self.src_tok_emb(src)), src_mask
+            )
+        def decode(self,tgt,memory,tgt_mask):
+            return self.transformer.decoder(
+                self.positional_encoding(self.tgt_tok_emb(tgt)), memory, tgt_mask
+            )
+
+        
